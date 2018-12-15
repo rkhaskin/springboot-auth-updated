fcff8bcd2c1e6b6de2964904699b47d44f0d5dbf
Rostislav Khaskin <rkhaskin@yahoo.com>
2018-12-15 10:00:44 -0500

    added security

--- src/main/java/com/auth0/samples/authapi/springbootauthupdated/SpringbootAuthUpdatedApplication.java
+++ src/main/java/com/auth0/samples/authapi/springbootauthupdated/SpringbootAuthUpdatedApplication.java
@@ -2,6 +2,8 @@ package com.auth0.samples.authapi.springbootauthupdated;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SpringbootAuthUpdatedApplication {
@@ -9,4 +11,10 @@ public class SpringbootAuthUpdatedApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootAuthUpdatedApplication.class, args);
 	}
+	
+	@Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
